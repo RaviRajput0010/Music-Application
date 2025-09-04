28115ff38576e60f96f1e95cfd7dec0b023333a8
RaviRajput0010 <rr935370@gmail.com>
2025-09-04 13:43:35 +0530

    Update app.py

--- JioSaavnAPI-master/JioSaavnAPI-master/app.py
+++ JioSaavnAPI-master/JioSaavnAPI-master/app.py
@@ -170,4 +170,5 @@ def result():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5100))  # use Render's PORT if available
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
+
